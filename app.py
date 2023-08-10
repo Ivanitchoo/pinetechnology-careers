f5b770e839de2c206f2683be98c43170b5835dea
Ivanitchoo <80470809+Ivanitchoo@users.noreply.github.com>
2023-08-10 12:12:07 +0000

    added responsiveness to homepage

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from database import carregar_vagas_from_db, carregar_vaga_from_db
 
 app = Flask(__name__)
@@ -28,6 +28,19 @@ def visualizar_vaga(id):
   else:
     return render_template('vaga.html', vaga=vaga_disponivel)
 
+
+@app.route("/vaga/<id>/candidatura_form")
+def candidatura_form(id):
+  
+  vaga_disponivel = carregar_vaga_from_db(id)
+  return render_template('candidatura_form.html', vaga=vaga_disponivel)
+
+@app.route("/vaga/<id>/candidatura_form/submit", methods =['post'])
+def candidatar_to_job(id):
+  data = request.form
+  return jsonify(data)
+  #Guardar na base de dados
+  #Enviar Email
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
